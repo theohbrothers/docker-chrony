6486000f5a11ebcd153afc776663b75a75add5d1
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2022-09-19 18:51:06 +0000

    Feature: Add ca-certificates

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -1,6 +1,7 @@
 @"
 FROM $( $VARIANT['_metadata']['distro'] ):$( $VARIANT['_metadata']['distro_version'] )
 
+RUN apk add --no-cache ca-certificates
 RUN apk add --no-cache $( $VARIANT['_metadata']['package'] )=$( $VARIANT['_metadata']['package_version'] )
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 RUN chmod +x docker-entrypoint.sh
